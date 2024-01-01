a776caee4b66587b8f14b87e780b2db8ea802de2
Ramkumar M <ramkumarmani2000@gmail.com>
2024-01-01 11:13:26 +0530

    Home page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { LoaderComponent } from './Component/loader/loader.component';
+import { AboutComponent } from './Component/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
